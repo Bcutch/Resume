d003c20c2c658734cbb2c3f5717d5524acc4e4d2
Bcutch <bcutch13@gmail.com>
2026-03-14 17:54:06 -0400

    fixed headshot

--- src/components/headShot.component.tsx
+++ src/components/headShot.component.tsx
@@ -6,18 +6,28 @@ interface HSProps {
 
 export const HeadShot: React.FC<HSProps> = ({
     position="flex flex-auto justify-center mt-5 ml-3",
-    size="28",
+    size=null,
     border="border-4 border-blue-600 ring-4 ring-blue-100",
 }) => {
 
     return (
         <>
-            <div className={position}>
-                <img
-                    src="/images/headshot.jpg"
-                    className={`h-${size} w-${size} ${border} aspect-square rounded-full object-cover shadow-lg`}
-                />
-            </div>
+            {size == null && (
+                <div className={position}>
+                    <img
+                        src="/images/headshot.jpg"
+                        className={`w-28 h-28 sm:w-36 sm:h-36 md:w-44 md:h-44 ${border} aspect-square rounded-full object-cover shadow-lg`}
+                    />
+                </div>
+            )}
+            {size != null && (
+                <div className={position}>
+                    <img
+                        src="/images/headshot.jpg"
+                        className={`h-${size} w-${size} ${border} aspect-square rounded-full object-cover shadow-lg`}
+                    />
+                </div>
+            )}
         </>
     )
 }
